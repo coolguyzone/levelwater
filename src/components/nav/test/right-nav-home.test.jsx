77ede63377808e1b5697b8b05b7ad0ef674a0a82
Sasha Berkowitz <sasha.b510@gmail.com>
2017-06-20 18:36:06 -0700

    linted nav and signup. nav css has some commented code;

--- src/components/nav/test/right-nav-home.test.jsx
+++ src/components/nav/test/right-nav-home.test.jsx
@@ -3,7 +3,6 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import RightNavHome from '../right-nav-home.jsx';
 
-
 describe('right nav home', () => {
   it('renders a snapshot', () => {
     const tree = shallow(<RightNavHome />);
