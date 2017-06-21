68cd4d96db0ab600c556341bfeb89427bd69f2f3
Sasha Berkowitz <sasha.b510@gmail.com>
2017-06-20 18:37:06 -0700

    linted reducers

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,9 +1,9 @@
 const login = (state = null, action) => {
   switch (action.type) {
-    case 'LOG_USER_IN':
-      return action.payload;
-    default:
-      return state;
+  case 'LOG_USER_IN':
+    return action.payload;
+  default:
+    return state;
   }
 };
 
